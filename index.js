4bfff40c283488f7bbc47e6631adb216260b7c6f
Susil Thapa <thapasusil53@gmail.com>
2024-01-27 12:31:07 +0545

    Simple chat app created

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ import { createServer } from "node:http";
 import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
 
+import { Server as SocketServer } from "socket.io";
+
 const app = express();
 const server = createServer(app);
+
+const io = new SocketServer(server);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -12,6 +16,12 @@ app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"));
 });
 
+io.on("connection", (socket) => {
+  console.log("======= Connected =========");
+  socket.on("chat message", (message) => {
+    io.emit("chat message", message);
+  });
+});
 server.listen(3000, () => {
   console.log("**** server running at http://localhost:3000 ****");
 });
